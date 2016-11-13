522ce6f1328dba734e9c83a465906da5915ab0bf
Bcole <blcole1@hotmail.com>
2016-11-12 19:27:07 -0500

    Testing

---
+++ Games/Controllers/WelcomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Games.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public string Index()
+        {
+            return "This is my <b>welcome</b> action...";
+        }
+
+        // 
+        // GET: /Welcome/Hello/ 
+
+        public string Hello(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
